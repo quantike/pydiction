3a4f22f3c71a822c55f45644d9ac305edc1892ef
quantike <55246750+quantike@users.noreply.github.com>
2024-10-16 16:03:57 -0500

    refactor: improve login flow

--- src/pydiction/client.py
+++ src/pydiction/client.py
@@ -1,3 +1,4 @@
+from typing import Dict
 import requests
 
 from pydiction.auth import Authenticator
@@ -9,11 +10,25 @@ class KalshiClient:
     def __init__(self, state: State, auth: Authenticator) -> None:
         self.state = state
         self.auth = auth
-        self.token = self.auth.get_auth_headers_rest(self.state.rest_base_url)
+        self.logged_in = self._login()
+
+    def _login(self) -> bool:
+        # Retrieve the response body from a login attempt
+        login_response: Dict[str, str] = self.auth.get_auth_headers_rest(self.state.rest_base_url)
+
+        # If the "token" response object exists, return True
+        if login_response.get("token"):
+            return True
+
+        # Else, login failed
+        return False
 
     def get_exchange_schedule(self):
+        """
+        Requests the exchange schedule.
+        """
         method = "GET"
-        path = "/exchange/schedule"
+        path = "/trade-api/v2/exchange/schedule"
 
         headers = self.auth.create_headers(method, path)
 
@@ -26,15 +41,41 @@ class KalshiClient:
         WARNING: This is an undocumented endpoint I happened upon.
         """
         method = "GET"
-        path = "/exchange/status"
+        path = "/trade-api/v2/exchange/status"
 
         headers = self.auth.create_headers(method, path)
 
         return requests.get(self.state.rest_base_url + path, headers=headers)
+
     def get_exchange_announcements(self):
+        """
+        Requests exchange announcements, if any.
+        """
         method = "GET"
-        path = "/exchange/announcements"
+        path = "/trade-api/v2/exchange/announcements"
 
         headers = self.auth.create_headers(method, path)
 
         return requests.get(self.state.rest_base_url + path, headers=headers)
+
+    def get_portfolio_balance(self):
+        """
+        Requests the portfolio balance for a logged-in user. Returns the balance in dollar cents, 
+        and the available payout in dollar cents.
+        """
+        method = "GET"
+        path = "/trade-api/v2/portfolio/balance"
+
+        headers = self.auth.create_headers(method, path)
+
+        return requests.get(self.state.rest_base_url + path, headers=headers)
+
+
+if __name__ == '__main__':
+    state = State()
+    auth = Authenticator(state)
+
+    client = KalshiClient(state, auth)
+
+    print(client.get_exchange_status())
+    print(client.get_portfolio_balance())
