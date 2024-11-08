2cbb4c0d0ada10a8f8143392e037909664b279df
quantike <55246750+quantike@users.noreply.github.com>
2024-11-08 15:11:36 -0600

    feat: add update function for tick so we can update values in-place

--- packages/common/src/common/models/ticker.py
+++ packages/common/src/common/models/ticker.py
@@ -4,12 +4,12 @@ from typing import Any, Dict
 
 
 @dataclass
-class Ticker:
+class Tick:
     """
-    Represents a ticker message that provides the current list price for a market.
+    Represents a tick message that provides the current list price for a market.
 
     Attributes:
-        ts(datetime): The timestamp of when the ticker update happened.
+        ts(datetime): The timestamp of when the tick update happened.
         price(int): The last price in the market when the ticker update happened. Between 1 and 99 (inclusive).
         bid(int): Current best bid on the "Yes" market. Between 1 and 99 (inclusive).
         ask(int): Current best ask on the "Yes" market. Between 1 and 99 (inclusive).
@@ -27,6 +27,37 @@ class Ticker:
     oi: int
     dollar_volume: int
     dollar_oi: int
+
+    def update(self, ts: datetime, price: int, bid: int, ask: int, volume: int, oi: int, dollar_volume: int, dollar_oi: int) -> None:
+        """
+        Updates the `Tick` dataclass in-place if there is a change. We may want to check if this is faster than just a full re-write of the data.
+
+        Attributes:
+            ts(datetime): The timestamp of when the tick update happened.
+            price(int): The last price in the market when the ticker update happened. Between 1 and 99 (inclusive).
+            bid(int): Current best bid on the "Yes" market. Between 1 and 99 (inclusive).
+            ask(int): Current best ask on the "Yes" market. Between 1 and 99 (inclusive).
+            volume(int): Total volume of individual contracts traded on the market so far.
+            oi(int): Number of active contracts in the market currently.
+            dollar_volume(int): Number of dollars traded on the market so far.
+            dollar_oi(int): Number of dollars positioned in the market current.
+        """
+        if self.ts != ts:
+            self.ts = ts
+        if self.price != price:
+            self.price = price
+        if self.bid != bid: 
+            self.bid = bid
+        if self.ask != ask:
+            self.ask = ask
+        if self.volume != volume:
+            self.volume = volume
+        if self.oi != oi:
+            self.oi = oi
+        if self.dollar_volume != dollar_volume:
+            self.dollar_volume = dollar_volume
+        if self.dollar_oi != dollar_oi:
+            self.dollar_oi = dollar_oi
 
     def to_dict(self) -> Dict[str, Any]:
         return {
