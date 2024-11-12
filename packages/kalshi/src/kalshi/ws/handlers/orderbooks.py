8fba4e6cb500da429647e551789e888310d037dd
quantike <55246750+quantike@users.noreply.github.com>
2024-11-12 12:55:03 -0600

    fix: fixed ask pricing, we want our Orderbook to always have a YES perspective, so to get the YES ask prices we need to take 100-no_bids

--- packages/kalshi/src/kalshi/ws/handlers/orderbooks.py
+++ packages/kalshi/src/kalshi/ws/handlers/orderbooks.py
@@ -21,27 +21,34 @@ class KalshiOrderbookHandler:
         try:
             match update_type:
                 case "orderbook_snapshot":
+                    # Our orderbook takes the "YES" perspective on the market. This means we interpret the "NO"
+                    # best bid as the best ask for "YES". This is done by taking 100 - yes_bid_price.
                     bids = [Level(level[0], level[1]) for level in data["yes"]]
-                    asks = [Level(level[0], level[1]) for level in data["no"]]
+                    asks = [Level(100-level[0], level[1]) for level in data["no"]]
 
                     # Build whole new book in the event that the current orderbook is empty
                     self.orderbook = Orderbook(bids=bids, asks=asks)
 
                     # Log the successful processing of the snapshot
                     logger.info(
-                        f"Orderbook created: f{self.orderbook.bba} mid {self.orderbook.mid_price} micro {self.orderbook.micro_price} spread {self.orderbook.spread},"
+                        f"Orderbook created: {self.orderbook.bba} mid {self.orderbook.mid_price} micro {self.orderbook.micro_price} spread {self.orderbook.spread},"
                     )
 
                 case "orderbook_delta":
-                    delta = Delta(price=data["price"], delta=data["delta"])
-
                     if data["side"] == "yes":
+                        delta = Delta(price=data["price"], delta=data["delta"])
                         self.orderbook.update(self.orderbook.bids, delta)
-                        logger.info(f"Orderbook YES update: f{self.orderbook.bba}")
+                        logger.info(
+                            f"Orderbook YES update: {self.orderbook.bba} mid {self.orderbook.mid_price} micro {self.orderbook.micro_price} spread {self.orderbook.spread}"
+                        )
 
                     elif data["side"] == "no":
+                        # Take the "YES" perspective and create the synthetic "YES" ask of 100-no_price
+                        delta = Delta(price=100-data["price"], delta=data["delta"])
                         self.orderbook.update(self.orderbook.asks, delta)
-                        logger.info(f"Orderbook NO update: f{self.orderbook.bba}")
+                        logger.info(
+                            f"Orderbook NO update: {self.orderbook.bba} mid {self.orderbook.mid_price} micro {self.orderbook.micro_price} spread {self.orderbook.spread}"
+                        )
 
         except Exception as e:
             raise Exception(f"Tick process error: {e}")
