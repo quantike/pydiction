4a6eccc3006d765c5f7c07e7e3f8c849a99ed87f
quantike <55246750+quantike@users.noreply.github.com>
2024-11-19 13:17:27 -0600

    fix: update Kalshi orderbook processing to handle sequence updates

--- packages/kalshi/src/kalshi/ws/handlers/orderbooks.py
+++ packages/kalshi/src/kalshi/ws/handlers/orderbooks.py
@@ -19,6 +19,12 @@ class KalshiOrderbookHandler:
             data(dict): A dictionary that represents the data from a `trade` message.
         """
         try:
+            seq = data.get("seq")
+
+            if seq is None:
+                logger.error(f"Failed to find sequence in orderbook update: {data}")
+                raise ValueError("Sequence number is missing in the orderbook data message")
+
             match update_type:
                 case "orderbook_snapshot":
                     # Our orderbook takes the "YES" perspective on the market. This means we interpret the "NO"
@@ -26,8 +32,9 @@ class KalshiOrderbookHandler:
                     bids = [Level(level[0], level[1]) for level in data["yes"]]
                     asks = [Level(100 - level[0], level[1]) for level in data["no"]]
 
-                    # Build whole new book in the event that the current orderbook is empty
-                    self.orderbook = Orderbook(bids=bids, asks=asks)
+                    # Refresh the book with the new snapshot data
+                    self.orderbook.refresh("bids", bids, seq)
+                    self.orderbook.refresh("asks", asks, seq)
 
                     # Log the successful processing of the snapshot
                     logger.info(
@@ -37,7 +44,7 @@ class KalshiOrderbookHandler:
                 case "orderbook_delta":
                     if data["side"] == "yes":
                         delta = Delta(price=data["price"], delta=data["delta"])
-                        self.orderbook.update(self.orderbook.bids, delta)
+                        self.orderbook.update(self.orderbook.bids, delta, seq)
                         logger.info(
                             f"Orderbook YES update: {self.orderbook.bba} mid {self.orderbook.mid_price} micro {self.orderbook.micro_price} spread {self.orderbook.spread}"
                         )
@@ -45,7 +52,7 @@ class KalshiOrderbookHandler:
                     elif data["side"] == "no":
                         # Take the "YES" perspective and create the synthetic "YES" ask of 100-no_price
                         delta = Delta(price=100 - data["price"], delta=data["delta"])
-                        self.orderbook.update(self.orderbook.asks, delta)
+                        self.orderbook.update(self.orderbook.asks, delta, seq)
                         logger.info(
                             f"Orderbook NO update: {self.orderbook.bba} mid {self.orderbook.mid_price} micro {self.orderbook.micro_price} spread {self.orderbook.spread}"
                         )
