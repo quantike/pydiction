b89f478a317293aa42b8f985209a9b0c29223e4a
quantike <55246750+quantike@users.noreply.github.com>
2024-12-27 01:11:49 -0600

    chore: update main

--- src/pydiction/main.py
+++ src/pydiction/main.py
@@ -7,10 +7,10 @@ from kalshi.rest import KalshiRestClient
 from kalshi.ws.client import KalshiWsClient
 from loguru import logger
 
-
 async def main():
     state = None
     kalshi_status_checker: KalshiStatus | None = None
+    websocket: KalshiWsClient | None = None
     CentralizedLogger(log_file="pydiction.log")
 
     try:
@@ -21,15 +21,13 @@ async def main():
         kalshi_status_checker = KalshiStatus.from_api(rest_client=api)
 
         websocket = KalshiWsClient(state)
-
-        await websocket.connect()
+        await websocket.start()
         await websocket.add_subscription(
             ["ticker", "trade", "orderbook_delta", "market_lifecycle"]
         )
 
         await asyncio.gather(
             state.refresh(),
-            websocket.monitor_connection_health(),
             kalshi_status_checker.run(),
         )
 
@@ -37,15 +35,13 @@ async def main():
         logger.critical("pydiction manually cancelled by user")
 
     except Exception as e:
-        logger.critical(f"pydiction encountered and unexpected error: {e}")
+        logger.critical(f"pydiction encountered an unexpected error: {e}")
         raise
 
     finally:
-        # TODO: Documenting that we have an unbound error for the processes that we are initializing within the try statement.
-        # Right now, they shouldn't be constructed outside of the try statement, but we may want to do this. We need to be able
-        # to call the `.shutdown()` function for all of these processes in order to close the program successfully.
-        logger.info("pydiction successfully shutdown")
-
+        if websocket:
+            await websocket.end()
+        logger.success("pydiction successfully shutdown")
 
 if __name__ == "__main__":
     asyncio.run(main())
